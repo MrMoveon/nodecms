0df22330b3ddc19d83da3026f0cd823f59f3b240
530385371@qq.com <3536024>
2018-03-20 22:03:36 +0800

    完善文件上传

--- app/controller/api.js
+++ app/controller/api.js
@@ -8,32 +8,52 @@ const awaitWriteStream = require('await-stream-ready').write;
 const sendToWormhole = require('stream-wormhole');
 
 class ApiController extends Controller {
-
+    // 上传接口
     async upload() {
+        // 读取文件流
         const stream = await this.ctx.getFileStream();
-       
-        const filename = encodeURIComponent(stream.fields.name) + path.extname(stream.filename).toLowerCase();
-        const target = path.join(this.config.baseDir, 'app/public/upload', filename);
+        // 上传的目录
+        var dir =path.join(this.config.baseDir, 'app/public/upload/'+new Date().toLocaleDateString());
+        //检测目录是否存在 不存在创建目录
+        if(!this.fsExistsSync(dir)){
+            await fs.mkdir(dir);
+        }
+        // 文件名
+        const filename = new Date().getTime() + path.extname(stream.filename).toLowerCase();
+        // 上传到的目标
+        const target = path.join(dir, filename);
+        // 写入流
         const writeStream = fs.createWriteStream(target);
         try {
             await awaitWriteStream(stream.pipe(writeStream));
         } catch (err) {
             await sendToWormhole(stream);
             throw err;
+            // 接口返回失败
+            return this.ctx.body={
+                code:0,
+                msg:'上传失败',
+                data:{}
+            }
         }
-        console.log(filename)
+        // 接口返回成功
         return this.ctx.body={
             code:0,
             msg:'上传成功',
             data:{
-                src:'/public/upload/' + filename
+                src:path.join('/public/upload/',new Date().toLocaleDateString(),filename)
             }
         }
-
-
-       
     }
-
+    //检测文件或者文件夹存在
+    fsExistsSync(path) {
+        try{
+            fs.accessSync(path,fs.F_OK);
+        }catch(e){
+            return false;
+        }
+        return true;
+    }
 }
 
 module.exports = ApiController;
